1a8764c3441d38c55e6df7efd7c03bc09d347e70
105052 <Sriraman.Srinivasan@merrillcorp.com>
2017-10-06 11:16:17 +0530

    initial checkin

---
+++ PPT2PDF/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PptToPdfConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = new List<string>()
+                            {
+                                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "08.ppt"),
+                                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "09.pptx")
+                            };
+
+
+            var outDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Out");
+            Directory.CreateDirectory(outDir);
+            var pdfConverter = new PdfConverter(outDir);
+
+            foreach (var f in files)
+            {
+                if (!pdfConverter.Process(f))
+                {
+                    Console.WriteLine("Failed.");
+                }
+            }
+
+            Console.WriteLine("Process Completed. Hit enter to Exit.");
+            Console.Read();
+        }
+    }
+}
---
+++ PPT2PDF/PptToPdfConverter/PdfConverter.cs
@@ -0,0 +1,52 @@
+﻿using Aspose.Slides;
+using System;
+using System.IO;
+
+namespace PptToPdfConverter
+{
+    public class PdfConverter
+    {
+        private readonly string OutDir;
+
+        public PdfConverter(string outDir)
+        {
+            this.OutDir = outDir;
+        }
+
+
+        public bool Process(string inFilePath)
+        {
+            try
+            {
+                var fi = new FileInfo(inFilePath);
+                var fileName = fi.Name;
+                var fileBaseName = Path.GetFileNameWithoutExtension(inFilePath);
+                var ext = Path.GetExtension(inFilePath);
+                var outPdf = Path.Combine(OutDir, $"{fileBaseName}.pdf");
+
+                Console.Write($"Processing {fileName} ... ");
+
+                if (ext != ".ppt" && ext != ".pptx")
+                {
+                    Console.WriteLine("Invalid filetype.");
+                    return false;
+                }
+
+                // Load the document
+                var ppDoc = new Presentation(inFilePath);
+                // Save the document in output format.
+                ppDoc.Save(outPdf, Aspose.Slides.Export.SaveFormat.Pdf);
+
+                Console.WriteLine("Completed.");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+
+}
